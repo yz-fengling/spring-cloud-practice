afe922bca480734f70db2e1b62b596e275519594
fengling <z2569126731@163.com>
2021-06-08 21:16:32 +0800

    success-springclo-practice

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/yz/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,98 @@
+package com.yz.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.yz.springcloud.entities.CommonResult;
+import com.yz.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() throws InterruptedException {
+
+        log.info(Thread.currentThread().getName()+"\t"+"...testA");//<----
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+
+        return "------testB";
+    }
+
+    //===============Sentinel降级-RT=====================
+    @GetMapping("/testD")
+    public String testD() {
+        /*try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        int age = 10 / 0;
+        log.info("testD 测试RT");
+        return "----------test D";
+    }
+
+    //===================Sentinel降级-异常数=================
+    //访问http://localhost:8401/testE，第一次访问绝对报错，因为除数不能为零，我们看到error窗口，但是达到5次报错后，进入熔断后降级。
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    //=================Sentinel热点key========================
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler  = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+
+    /*兜底方法*/
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
+
+//======================SentinelResource配置=============================
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020,"serial001"));
+    }
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl()
+    {
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020,"serial002"));
+    }
+
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,//<-------- 自定义限流处理类
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler()
+    {
+        return new CommonResult(200,"按客戶自定义",new Payment(2020,"serial003"));
+    }
+
+}
